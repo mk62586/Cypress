553b861b225039e2e9c735eaf74cc0f37c900795
mk62586 <89150941+mk62586@users.noreply.github.com>
2022-04-26 16:54:57 +0530

    correct the code

--- cypress/integration/MyStore/MyStoreTestCases/Registration/Tc02_login.spec.ts
+++ cypress/integration/MyStore/MyStoreTestCases/Registration/Tc02_login.spec.ts
@@ -3,34 +3,37 @@
 import MyStore from '../../../../PageObject/login.page';
 
 describe("Login the page",()=>{
-
     let m :any
-
     beforeEach("fixture",()=>{
-      cy.visit("/")
-       cy.fixture("example").then((user)=>{
- m = user
+      cy.fixture("example").then((user)=>{
+      m = user
        })
     })
-    it.only("Login with password",()=>{
-      MyStore.email(m.email)
-      MyStore.password(m.pass)
-      MyStore.submit()
+    beforeEach("visit to my store",()=>{
+      cy.visit(m.url)
     })
+    it.only("Login with password",()=>{
+      MyStore.email().type(m.email)
+      MyStore.password().type(m.pass)
+      MyStore.submit().click()
+    })
+    it.only("visual test case of login",()=>{
+      cy.document().toMatchImageSnapshot();
+  })
    it.skip("Login  without email and password",()=>{
-      MyStore.submit()
+      MyStore.submit().click()
       MyStore.login1().should("be.visible")
     })
     it.skip("Login with  right email and wrong password",()=>{
-      MyStore.email(m.email)
-      MyStore.password(m.pass3)
-      MyStore.submit()   
+      MyStore.email().type(m.email)
+      MyStore.password().type(m.pass3)
+      MyStore.submit().click()  
       MyStore.login2().should("be.visible")     
   })
   it("Login with wrong email and right password",()=>{
-    MyStore.email(m.email3)
-    MyStore.password(m.pass)
-    MyStore.submit()
-    MyStore.login2().should("be.visible")
+      MyStore.email().type(m.email3)
+      MyStore.password().type(m.pass)
+      MyStore.submit().click()
+      MyStore.login2().should("be.visible")
  })
 })
